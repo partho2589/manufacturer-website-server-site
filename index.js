9f179b4983fa4104b0c9e23f3e30de44c7d44365
partho2589 <parthomalo615@.com>
2022-05-24 11:08:58 +0600

    added product id

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ async function run() {
             res.send(products)
         } )
 
+        app.get('/product/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query)
+            res.send(product)
+        })
+
     }
     finally {
 
